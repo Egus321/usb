f415dd20d1f657da5b4f68956c0728783d221374
Egus321 <118482702+Egus321@users.noreply.github.com>
2026-06-24 00:12:24 +0300

    windows script
    [+] windows script

---
+++ detect.ps1
@@ -0,0 +1,15 @@
+try {
+    Add-Type -AssemblyName PresentationFramework -ErrorAction Stop
+    [System.Windows.MessageBox]::Show('hi from usb') | Out-Null
+    exit 0
+} catch {
+    try {
+        Add-Type -AssemblyName System.Windows.Forms -ErrorAction Stop
+        [System.Windows.Forms.MessageBox]::Show('hi from usb') | Out-Null
+        exit 0
+    } catch {
+        $shell = New-Object -ComObject WScript.Shell
+        $shell.Popup('hi from usb', 0, 'Message', 0) | Out-Null
+        exit 0
+    }
+}
